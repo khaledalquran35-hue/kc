b20b6c9417ab04b576d4d94a3fe163d52d65328c
lad <khaledalquran35@gmail.com>
2026-09-07 15:34:48 +0300

    Add new HTML pages for About Us, Sign In, and Contact Us
    - Created p.html for the About Us section with a welcome message.
    - Created s.html for the Sign In page, including fields for email, username, password, and year, styled with Bootstrap.
    - Created y.html for the Contact Us section with a message inviting inquiries.

---
+++ final/k.js
@@ -0,0 +1,17 @@
+const kj=document.getElementById("button");
+kj.style.backgroundColor="black";
+kj.style.color="white";
+  
+
+const h=document.getElementsByClassName("lk");
+h.innerHTML="Sign In";
+h.style.color="black";
+
+
+
+const lo=document.getElementById("Contact");
+lo.addEventListener("click",function(){
+    lo.innerHTML="Contact";
+    style.backgroundColor="black";
+    style.color="white";
+});
